ac3df10db2b49d4b12faf6a2e72016190ac7378f
Katsuki <Katsuki_Sato0130@icloud.com>
2020-08-30 10:12:14 +0900

    Remove 404/design/

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
-import { MainTitle, Ghost, SubTitle } from './design'
 import Layout from '../../components/Layout'
 import Wrapper from '../../components/Wrapper'
 import SEO from '../../components/SEO'
 import RelatedPosts from '../../components/RelatedPosts'
 import { Text } from '../../components/Commons'
+import styled from 'styled-components'
+
+const MainTitle = styled.h1`
+  line-height: 1.5;
+  text-align: center;
+  font-size: 3rem;
+`
+
+const Ghost = styled.span`
+  display: block;
+  line-height: 1.5;
+  text-align: center;
+  font-size: 7rem;
+`
+
+const SubTitle = styled.h2`
+  padding-top: 40px;
+  line-height: 1.2;
+  border-top: 1px solid #ececec;
+  margin-top: 44px;
+`
 
 interface Props {
   location: string
