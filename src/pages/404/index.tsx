8f6c1debb35df6d55e173bc1de566815dd46a4da
Katsuki <Katsuki_Sato0130@icloud.com>
2020-09-04 07:52:20 +0900

    Fix 404page

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -2,21 +2,8 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import Wrapper from '../../components/Wrapper'
 import SEO from '../../components/SEO'
-import { Text } from '../../components/Commons'
-import styled from 'styled-components'
-
-const MainTitle = styled.h1`
-  line-height: 1.5;
-  text-align: center;
-  font-size: 3rem;
-`
-
-const Ghost = styled.span`
-  display: block;
-  line-height: 1.5;
-  text-align: center;
-  font-size: 7rem;
-`
+import Hero from '../../components/Hero'
+import { Container, Content, MainTitle, Ghost, Text } from './styles'
 
 interface Props {
   location: string
@@ -26,15 +13,21 @@ const NotFoundPage = ({ location }: Props) => {
   return (
     <Layout location={location} noCover={true}>
       <SEO title="Page Not Found" />
+      <Hero title="404 Page Not Found" />
+
       <Wrapper>
-        <MainTitle>404 Page Not Found</MainTitle>
-        <Ghost role="img" aria-label="Ghost">
-          👻
-        </Ghost>
-        <Text>
-          Looks like you've followed a broken link or entered a URL that doesn't
-          exist on this site.
-        </Text>
+        <Container>
+          <Content>
+            <MainTitle>404 Page Not Found</MainTitle>
+            <Ghost role="img" aria-label="Ghost">
+              👻
+            </Ghost>
+            <Text>
+              Looks like you've followed a broken link or entered a URL that
+              doesn't exist on this site.
+            </Text>
+          </Content>
+        </Container>
       </Wrapper>
     </Layout>
   )
